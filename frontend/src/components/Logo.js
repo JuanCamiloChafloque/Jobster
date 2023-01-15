d5c1525aa15abfb346fa7260f2b6e02da514a086
Camilo <camilochafloque@gmail.com>
2023-01-15 11:11:58 -0600

    Landing page finished with styles

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,14 @@
+import icon from "../assets/images/icon.png";
+
+import React from "react";
+
+const Logo = () => {
+  return (
+    <>
+      <img src={icon} alt="icon" className="logo" />
+      <h3>Jobster</h3>
+    </>
+  );
+};
+
+export default Logo;
--- frontend/src/screens/Landing.js
+++ frontend/src/screens/Landing.js
@@ -1,7 +1,74 @@
 import React from "react";
+import styled from "styled-components";
+import main from "../assets/images/logo.svg";
+import Logo from "../components/Logo";
 
 const Landing = () => {
-  return <h1>Landing</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h3>
+            Job <span>Tracking</span> App
+          </h3>
+          <p>
+            An app that helps students and graduates keep track of their
+            progress during their job hunting process by seeing their stats
+            based on their pending, completed and rejected
+            interviews/applications
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="main" />
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+    h3 {
+      color: var(--primary-500);
+      margin-left: 5px;
+      margin-top: 25px;
+    }
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h3 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 2rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
 
 export default Landing;
