19e255f992b5821b9b45d9c84487ab1b0e51f945
Camilo <camilochafloque@gmail.com>
2023-01-16 11:47:47 -0600

    Job model created

---
+++ backend/models/Job.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Please provide company name"],
+      maxLength: 50,
+    },
+
+    position: {
+      type: String,
+      required: [true, "Please provide position name"],
+      maxLength: 100,
+    },
+
+    status: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+
+    jobType: {
+      type: String,
+      enum: ["full-time", "part-time", "remote", "internship"],
+      default: "full-time",
+    },
+
+    jobLocation: {
+      type: String,
+      required: [true, "Please provide job location"],
+      default: "city",
+    },
+
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide a user creator"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Job", JobSchema);
