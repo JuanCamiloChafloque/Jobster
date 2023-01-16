c8917c44d893327d421ae080d87d6552cf6bdd6e
Camilo <camilochafloque@gmail.com>
2023-01-15 18:16:30 -0600

    CORS and Proxy configured for app

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import dotenv from "dotenv";
+import cors from "cors";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 import connectDB from "./db/database.js";
@@ -14,8 +15,9 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-//JSON
+//JSON & CORS
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/jobs", jobRoutes);
