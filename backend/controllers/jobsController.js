21092ba85b1b1fb261d845f2f039ae2c6bbeb4c2
Camilo <camilochafloque@gmail.com>
2023-01-16 11:52:11 -0600

    Create job controller finished

--- backend/controllers/jobsController.js
+++ backend/controllers/jobsController.js
@@ -1,4 +1,18 @@
-export const createJob = async (req, res, next) => {};
+import Job from "../models/Job.js";
+import { StatusCodes } from "http-status-codes";
+import BadRequestError from "../errors/BadRequestError.js";
+import UnathenticatedError from "../errors/UnauthenticatedError.js";
+
+export const createJob = async (req, res, next) => {
+  const { position, company } = req.body;
+  if (!position || !company) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+};
 
 export const getAllJobs = async (req, res, next) => {};
 
