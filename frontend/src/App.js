459022e9434b02423486c5ace18f9ddbbb61663f
Camilo <camilochafloque@gmail.com>
2023-01-15 19:28:26 -0600

    Protecting routes and dashboard nested routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./screens/Dashboard";
 import Error from "./screens/Error";
 import Landing from "./screens/Landing";
 import Register from "./screens/Register";
+import AddJob from "./screens/Dashboard/AddJob";
+import AllJobs from "./screens/Dashboard/AllJobs";
+import Profile from "./screens/Dashboard/Profile";
+import Stats from "./screens/Dashboard/Stats";
+import SharedLayout from "./screens/Dashboard/SharedLayout";
+import ProtectedRoute from "./screens/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
---
+++ frontend/src/screens/Dashboard/SharedLayout.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import styled from "styled-components";
+
+const SharedLayout = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Link to="add-job">Add Job</Link>
+      </nav>
+      <Outlet />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  .dashboard {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+  .dashboard-page {
+    width: 90vw;
+    margin: 0 auto;
+    padding: 2rem 0;
+  }
+  @media (min-width: 992px) {
+    .dashboard {
+      grid-template-columns: auto 1fr;
+    }
+    .dashboard-page {
+      width: 90%;
+    }
+  }
+`;
+
+export default SharedLayout;
